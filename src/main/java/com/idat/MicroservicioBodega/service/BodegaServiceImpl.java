a86f7dded6af67c6ca4dc63b2f3fd4d51d9320f2
Josue Perez <jos22_98@hotmail.com>
2022-08-02 21:48:21 -0500

    agregar service

--- src/main/java/com/idat/MicroservicioBodega/service/BodegaServiceImpl.java
+++ src/main/java/com/idat/MicroservicioBodega/service/BodegaServiceImpl.java
@@ -3,6 +3,7 @@ package com.idat.MicroservicioBodega.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.idat.MicroservicioBodega.client.OpenFeignClient;
 import com.idat.MicroservicioBodega.model.Bodega;
@@ -12,7 +13,7 @@ import com.idat.MicroservicioBodega.model.ProductoBodegaFK;
 import com.idat.MicroservicioBodega.repository.BodegaRepository;
 import com.idat.MicroservicioBodega.repository.DetalleBodegaRepository;
 
-
+@Service
 public class BodegaServiceImpl implements BodegaService{
 	
 	@Autowired
